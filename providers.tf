b8fe4f37312db6cbb0e9b925310ecea73db06709
Ajay Kapoor <ajaykapoor2@yahoo.com>
2025-05-08 14:10:39 -0400

    Add CI workflow

--- providers.tf
+++ providers.tf
@@ -2,8 +2,8 @@
 # PROVIDERS
 ##################################################################################
 
-# provider "aws" {
-#   access_key = "AKIATCKATDIPOBBEL2UR"
-#   secret_key = "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"
-#   region     = "us-east-1"
-# }
+provider "aws" {
+  access_key = "AKIATCKATDIPOBBEL2UR"
+  secret_key = "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"
+  region     = "us-east-1"
+}
