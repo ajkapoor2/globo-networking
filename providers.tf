fd18a7d35da0391a9e9b444227ae427d2035ad88
Ajay Kapoor <ajaykapoor2@yahoo.com>
2025-05-08 13:38:42 -0400

    initial commit

---
+++ providers.tf
@@ -0,0 +1,9 @@
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "AKIATCKATDIPOBBEL2UR"
+  secret_key = "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"
+  region     = "us-east-1"
+}
